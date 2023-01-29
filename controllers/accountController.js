797e50dfc7371c5498d2fb6344b666faf6170b64
nguyenanhdung281101 <nguyenanhdung2811@gmail.com>
2023-01-29 21:04:47 +0700

    update

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -3,62 +3,81 @@ const utils = require("../utils");
 const moment = require("moment");
 const { generateRandomStr, sha256 } = require("../utils");
 const sendEmail = require("../utils/nodeMailer");
+const jwt = require("jsonwebtoken");
 
 const accountController = {
     signIn: async (req, res) => {
         try {
-            const account = await Account.findOne({
-                email: req.body.email,
-            })
+            let account;
+            if (req.headers.authorization) {
+                const accessToken = req.headers.authorization.split(" ")[1];
+
+                account = await Account.findOne({
+                    accessToken: accessToken,
+                });
+                if (!account) {
+                    return res.send({
+                        result: "failed",
+                        message: "Không đủ quyền truy cập",
+                    });
+                }
+
+                return res.send({
+                    result: "success",
+                    account: account,
+                });
+            } else {
+                account = await Account.findOne({
+                    email: req.body.email,
+                });
+            }
 
             if (!account) {
                 return res.status(404).json({
                     result: "failed",
                     message: "Tài khoản không tồn tại",
-                })
+                });
             }
 
-            const hashed = await utils.sha256(req.body.password)
-            const validPassword = hashed === account.password
+            const hashed = await utils.sha256(req.body.password);
+            const validPassword = hashed === account.password;
 
             if (!validPassword) {
                 return res.status(404).json({
                     result: "failed",
                     message: "Sai mật khẩu",
-                })
+                });
             }
 
-            if (
-                !account.accessToken ||
-                moment(account.expirationDateToken).diff(moment.now()) < 0
-            ) {
-                var accessToken = generateRandomStr(32)
-                var expirationDate = new Date()
-                var time = expirationDate.getTime()
-                var time1 = time + 24 * 3600 * 1000
-                var setTime = expirationDate.setTime(time1)
-                var expirationDateStr = moment(setTime)
-                    .format("YYYY-MM-DD HH:mm:ss")
-                    .toString()
+            let responseAccount;
 
-                await account.updateOne({
-                    accessToken: accessToken,
+            // if (!account.accessToken || moment(account.expirationDateToken).diff(moment.now()) < 0) {
+            var token = await jwt.sign({ email: req.body.email }, "IOT");
+            var expirationDate = new Date();
+            var time = expirationDate.getTime();
+            var time1 = time + 24 * 3600 * 1000;
+            var setTime = expirationDate.setTime(time1);
+            var expirationDateStr = moment(setTime).format("YYYY-MM-DD HH:mm:ss").toString();
+
+            responseAccount = await Account.findByIdAndUpdate(
+                account._id,
+                {
+                    accessToken: token,
                     expirationDateToken: expirationDateStr,
-                })
-            }
-            const responseAccount = await Account.findOne({
-                _id: account._id,
-            })
+                },
+                { new: true }
+            );
+            // }
 
             return res.send({
                 result: "success",
-                account: responseAccount.toJSON(),
-            })
+                account: responseAccount,
+            });
         } catch (err) {
             res.status(500).json({
                 result: "failed",
-                error: err,
-            })
+                error: err.message,
+            });
         }
     },
 
@@ -67,13 +86,13 @@ const accountController = {
             //check Account existent
             let account = await Account.findOne({
                 email: req.body.email,
-            })
+            });
 
             if (account) {
                 return res.send({
                     result: "failed",
                     message: "Tài khoản đã tồn tại",
-                })
+                });
             }
 
             const hashed = await utils.sha256(req.body.password);
@@ -85,67 +104,65 @@ const accountController = {
                 email: req.body.email,
                 password: hashed,
                 expirationDateToken: null,
-            })
+            });
 
-            await newAccount.save()
+            await newAccount.save();
 
             return res.send({
                 result: "success",
                 account: newAccount,
-            })
+            });
         } catch (err) {
             res.status(500).send({
                 result: "failed",
                 message: err,
-            })
+            });
         }
     },
 
     signOut: async (req, res) => {
         try {
-            const accessToken = req.headers.authorization.split(" ")[1]
+            const accessToken = req.headers.authorization.split(" ")[1];
             const account = await Account.findOne({
                 accessToken: accessToken,
-            })
+            });
             await account.updateOne({
                 accessToken: null,
                 expirationDateToken: null,
-            })
+            });
 
             const responseAccount = await Account.findOne({
                 _id: account._id,
-            })
+            });
             res.send({
                 result: "success",
-            })
+            });
         } catch (error) {
             res.status(500).send({
                 result: "failed",
                 reason: error.message,
-            })
+            });
         }
     },
 
     requestToResetPassword: async (req, res) => {
         try {
-            let { email } = req.body
+            let { email } = req.body;
 
             let account = await Account.findOne({
                 email: email,
-            })
+            });
 
             if (!account) {
                 return res.send({
                     result: "failed",
                     message: "email không hợp lệ",
-                })
+                });
             }
-            var random = 100000 + Math.random() * 900000
-            var plainResetPasswordToken = Math.floor(random)
+            var random = 100000 + Math.random() * 900000;
+            var plainResetPasswordToken = Math.floor(random);
 
-            const hashedResetPasswordToken = await utils.sha256(
-                plainResetPasswordToken.toString()
-            )
+            const hashedResetPasswordToken = await utils.sha256(plainResetPasswordToken.toString());
 
             // var expirationDate = new Date()
             // var time = expirationDate.getTime()
@@ -163,44 +180,39 @@ const accountController = {
                     password: hashedResetPasswordToken,
                     // expirationDateResetPasswordToken: expirationDateStr,
                 }
-            )
-
-            await sendEmail(
-                email,
-                "Đặt lại mật khẩu SmartGardent",
-                plainResetPasswordToken
             );
+
+            await sendEmail(email, "Đặt lại mật khẩu SmartGardent", plainResetPasswordToken);
 
             res.send({
                 result: "success",
                 // expirationDate: moment(expirationDate).toDate(),
-            })
-
+            });
         } catch (error) {
             res.send({
                 result: "failed",
                 message: error,
-            })
+            });
         }
     },
 
     resetPassword: async (req, res) => {
         try {
-            let { email, resetPasswordToken, newPassword } = req.body
+            let { email, resetPasswordToken, newPassword } = req.body;
 
             let account = await Account.findOne({
                 email: email,
-            })
+            });
 
-            const hashedResetPasswordToken = utils.sha256(resetPasswordToken)
+            const hashedResetPasswordToken = utils.sha256(resetPasswordToken);
 
-            const hashedPassword = utils.sha256(newPassword)
+            const hashedPassword = utils.sha256(newPassword);
 
             if (!account) {
                 return res.send({
                     result: "failed",
                     message: "Đổi mật khẩu không thành công",
-                })
+                });
             }
 
             if (account.resetPasswordToken === hashedResetPasswordToken) {
@@ -213,17 +225,17 @@ const accountController = {
                         expirationDateResetPasswordToken: null,
                         password: hashedPassword,
                     }
-                )
+                );
                 return res.send({
                     result: "success",
                     message: "Thay đổi mật khẩu thành công",
-                })
+                });
             }
         } catch (error) {
             res.send({
                 result: "failed",
                 message: error,
-            })
+            });
         }
     },
     changePassword: async (req, res) => {
@@ -232,34 +244,34 @@ const accountController = {
 
             const account = await Account.findOne({
                 accessToken: accessToken,
-            })
-            const password = await sha256(req.body.password)
-            const newPassword = await sha256(req.body.newPassword)
-            
+            });
+            const password = await sha256(req.body.password);
+            const newPassword = await sha256(req.body.newPassword);
+
             if (account) {
                 if (password === account.password) {
                     await Account.findByIdAndUpdate(account.id, {
                         password: newPassword,
-                    })
+                    });
                     return res.send({
                         result: "success",
                         message: "Đổi mật khẩu thành công",
-                    })
+                    });
                 }
                 return res.send({
                     result: "failed",
                     message: "Mật khẩu cũ không chính xác",
-                })
+                });
             }
             return res.send({
                 result: "faled",
                 message: "Sai email",
-            })
+            });
         } catch (err) {
             res.send({
                 result: "faled",
                 message: err,
-            })
+            });
         }
     },
 };
